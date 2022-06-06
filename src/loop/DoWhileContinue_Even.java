ec04e42a63c1546fd5e72dc6f4b60027047508f4
Jiwon316 <s92jw0316@gmail.com>
2022-06-06 17:26:25 +0900

    do-while+continue

--- src/loop/DoWhileContinue_Even.java
+++ src/loop/DoWhileContinue_Even.java
@@ -1,9 +1,9 @@
 package loop;
 
-public class Review {
+public class DoWhileContinue_Even {
 	public static void main(String[] args) {
 		int sum = 0;
-		int i = 1;
+		int i = 0;
 
 		do {		
 			i++; 
@@ -11,8 +11,9 @@ public class Review {
 				continue;
 			}
 			sum += i;	
-			}while (i <=10);
+			}while (i <=100);
 		// 컨티뉴를 사용하면 조건문으로 이동
 		System.out.println(sum);
 	}
 }
+//홀수와 짝수는 조건이 달라짐, 같은 식으로 실행할 경우 답이 나오지 않음
