769c4f293b6a4b3b7e4e9b38ea698f3d9529727d
Jiwon316 <s92jw0316@gmail.com>
2022-06-06 16:56:22 +0900

    do-while+continue

--- src/loop/Review.java
+++ src/loop/Review.java
@@ -4,15 +4,15 @@ public class Review {
 	public static void main(String[] args) {
 		int sum = 0;
 		int i = 1;
-		do {
-			sum += i;
-			i++;
-		} while (i <=10);
+
+		do {		
+			i++; 
+			if(i%2!=0) {
+				continue;
+			}
+			sum += i;	
+			}while (i <=100);
 		// 컨티뉴를 사용하면 조건문으로 이동
-
 		System.out.println(sum);
-
 	}
-
 }
-      
